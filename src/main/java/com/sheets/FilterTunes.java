b05a400f84707183ad2d8928b945ec620257aee7
ampyjoe <ampyjoe@gmail.com>
2021-02-12 10:18:42 -0800

    Added partial match for name

--- src/main/java/com/sheets/FilterTunes.java
+++ src/main/java/com/sheets/FilterTunes.java
@@ -33,7 +33,7 @@ public class FilterTunes {
     if (herokuPort != null) {
       return Integer.parseInt(herokuPort);
     }
-    return 7000;
+    return 7000;    // If not on Heroku
   }
     
     
@@ -45,77 +45,52 @@ public class FilterTunes {
         String audioTagPrefix = "<audio controls><source src=\"";
         String googlePrefix = "http://docs.google.com/uc?export=open&id=";
         String closeTag = "\"/></audio><br>\n";
-        
-        //.start(getHerokuAssignedPort())
+
         // Set up Javalin incl a dir for static files
         Javalin app = Javalin.create(config -> config.addStaticFiles("public")).start(getHerokuAssignedPort());
-        //Javalin app = Javalin.create(config -> config.addStaticFiles("public")).start(7000);
-        //Javalin app = Javalin.create().start(7000);
-        
 
-
-
-        // Start listening for requests
+        // Listen for POST to "/tunes"
         app.post("/tunes", ctx -> {
-            
-        
-        List<List<String>> tuneDataList;       // Error on line 271 if this declared as null????
-        //List<String> tempList = new ArrayList<>();
-        //FileUtil.
-        
-        String person = null;
+
+        List<List<String>> tuneDataList;
         try {
-            Path file = new File("target/classes/public/tunesdata.txt").toPath();
+            Path file = new File("target/classes/public/tunesdata.txt").toPath();   // Use Heroku-supported key/value setup
 
             Stream<String> lineDetail = Files.lines(file);
-            tuneDataList  = 
+            tuneDataList  =
                     lineDetail
                     .map((String l) -> {
                         return Arrays.asList(l.split("\\^",-2));    // 1.8 compatible
-                            })                            
+                            })
                     .collect(toList());
 
-                    
-        } catch (NoSuchElementException e) {
-            //System.out.println("No such user " + e);
-            throw new NoSuchElementException("No such user found");
-        } catch (IOException ex) { 
-            //System.out.println("Problem opening user data file.\n" + ex);
-            throw new IOException("Problem opening user data file");
+        } catch (IOException ex) {
+            throw new IOException("Problem opening data file");
         }
 
         List<String> headings = tuneDataList.get(0);
-        
         System.out.println("tuneDataList: " + headings);
-        
-        System.out.println("Index for Melodic Range: " + headings.indexOf("Melodic Range"));
-        
-        
 
-            
-        // Get the Predicate to use with the data from sheets
-        
+        // Get the Predicate to use with the data
         Predicate<List<String>> aFilter = getStringPredicate(ctx, headings);
-        
-                
+
         int finalPt = Integer.parseInt(Objects.requireNonNull(ctx.formParam("Performance Type")));  // Or return "1" if null by catching null?
 
-        
         // Now get the results and output as HTML links for the songs
-            // As with other parameters, no selections means ALL performances of a song selected
-            // TODO tidy up the filter lines
-        
-
+        // As with other parameters, no selections means ALL performances of a song selected
+        // TODO tidy up the filter lines
         
         String htmlOutput = tuneDataList.stream()
                 .skip(1)                                                    // skip headings line
-                .filter(t -> t.get(6 + finalPt) != null)                    // Check that a performance of appropriate type exists for this song
-                .filter(t -> t.get(6 + finalPt).startsWith("http"))         // And that it looks like a URL (check if valid URL??)
+                .filter(t -> t.get(6 + finalPt) != null)                    // Check that a performance of appropriate performance type exists for this song
+                .filter(t -> t.get(6 + finalPt).startsWith("http"))         // And that it looks like a URL (TODO actually check if valid URL??)
                 .filter(aFilter)
                .map(o -> (
                         o.get(headings.indexOf("Name")) 
                         + " ... "
                         + audioTagPrefix + o.get(6 + finalPt) + closeTag))
+
+                                                                            // TODO Sort the output?
                 .collect(joining());
         
             System.out.println("htmlOutput>>>>> " + htmlOutput);
@@ -128,28 +103,16 @@ public class FilterTunes {
 "        </form> <br> \n"
                      + htmlOutput);
         });
-        
-        
-//        get("/tunefilter.jsp", (req, res) -> {
-//        
-////            System.out.println("hello: " + req.servletPath());
-////        
-////            res.redirect("http://localhost:4567/public/tunefilter.jsp");
-//            return new ModelAndView("", "tunefilter.jsp");
-//        
-//        });
 
-               
+
     }
     
     static Predicate<List<String>> getStringPredicate(Context ctx, List<String> headings)  {
-        
-        //List<Object> headings = getHeadings();
-        
-               // Build the Filter to use with the data from sheets
+
+        // Build the Filter to use with the data from sheets
         Map<String, List<String>> queryParamMap = ctx.formParamMap();    // TODO Possibly try to remove any params with no values?
         
-        Set<Map.Entry<String, List<String>>> queryParams = queryParamMap.entrySet();
+        Set<Map.Entry<String, List<String>>> queryParams = queryParamMap.entrySet(); // Need a Set for iteration
         
         System.out.println("The params:" + queryParams);
 
@@ -163,22 +126,23 @@ public class FilterTunes {
         while(itr.hasNext()) 
         { 
              String key = itr.next().getKey();
+            System.out.println("Key before conditional: " + key);
               values = queryParamMap.get(key);
 
              Predicate<List<String>> orFilter = o -> false;          // necessary to initialize before each inner loop - each attribute value OR'd
-             if (values.size() > 0 && !values.get(0).equals("") && !key.equalsIgnoreCase("Performance Type")) {  // there are some values and the first isn't "" (so don't create predicate if nothing checked)
+
+             // there are some values and the first value isn't "" (so don't create a predicate if nothing checked)
+             if (values.size() > 0 && !values.get(0).equals("") && !key.equalsIgnoreCase("Performance Type")) {
                  for (String value : values) {
                      System.out.println("key: " + key + " value: " + value);
 
-                     System.out.println("Other key: " + key + " value: " + value);
-                     //if (key.equalsIgnoreCase("Melodic Range")) {
-                             for (String singleValue: value.split(":")) {
-                                 orFilter = orFilter.or((List<String> o) ->
-                                         o.get(headings.indexOf(key)).equalsIgnoreCase(singleValue));
-                             }
-
-//                     } else orFilter = orFilter.or((List<String> o) ->              // This might not be necessary as it's a special case of the if stmt
-//                             o.get(headings.indexOf(key)).equalsIgnoreCase(value));
+                     // A set of values for a single checkbox can be specified in HTML by using ":" to separate individual values
+                     // TODO add special case for "name" key so it uses contains() not equals()
+                     for (String singleValue: value.split(":")) {
+                         orFilter = orFilter.or((List<String> o) ->
+                                 !key.equalsIgnoreCase("Name")?o.get(headings.indexOf(key)).equalsIgnoreCase(singleValue):
+                                         o.get(headings.indexOf(key)).toLowerCase().contains(singleValue.toLowerCase()));
+                     }
 
                  }
                  andFilter = andFilter.and(orFilter);
@@ -186,22 +150,7 @@ public class FilterTunes {
             } 
             System.out.println("Perf type: " + pt);
 
-        } 
-
-        
+        }
         return andFilter;
-        
-        
     }
-    
-    
-//    // Temporary - maybe should just be a settings class?
-//    static List<Object> getHeadings() throws IOException, GeneralSecurityException{
-//        
-//
-//        
-//        return List.of("hello");
-//         
-//    } 
-
 }
